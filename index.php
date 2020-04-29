87721e58590bbb14907335f79f2f9bad0f990597
999burya999 <burmiistrov2012@yandex.ru>
2020-04-29 13:36:26 +0300

    Laba4

--- index.php
+++ index.php
@@ -7,3 +7,21 @@ include_once 'Burya/Linear.php';
 include_once 'Burya/Square.php';
 include_once 'Burya/BuryaException.php';
 include_once 'Burya/Log.php';
+
+$arr=array();
+
+$arr[] = readline("a= ");
+$arr[] = readline("b= ");
+$arr[] = readline("c= ");
+
+try {
+    $solver = new Alex\Square();
+	$roots = $solver->solve($arr[0], $arr[1], $arr[2]);
+
+    Alex\Log::log("roots: " . implode(" , ", $roots));
+   
+}catch(Alex\BuryaException $e) {
+
+    Alex\Log::log($e->getMessage());
+}
+Alex\Log::write();
